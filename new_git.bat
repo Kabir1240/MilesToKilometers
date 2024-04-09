838796afdf49a7992567c193662cbea28253b0e9
Kabir Kashif <kashif.kabir60@gmail.com>
2024-04-10 02:23:50 +0500

    added executable

--- new_git.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-git init
-git remote add origin https://github.com/Kabir1240/MilesToKilometers.git
-git branch -m master main
-git pull origin main
-git add .
-git commit -m "initial commit"
-git push -u origin main
